c52f8f0dc1bbd47e7ba9d9e94e0489c3e2226fa5
sajeel12 <msajeelahmad2001@gmail.com>
2023-06-09 00:30:49 +0500

    9June2023

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -53,6 +53,7 @@ export default function (state = initialState, action) {
             };
 
         case LOGOUT_SUCCESS:
+            localStorage.removeItem('token');
             return {
                 ...state,
                 token: null,
